89c38f8ccb6f1c8861739d5feb3f27d002dadf94
elias <elias.sabja@uc.cl>
2022-04-06 17:28:02 -0400

    Player random generation in canvas

--- pacman.js
+++ pacman.js
@@ -1,19 +1,37 @@
 const board = document.getElementById("board");
 const ctx = board.getContext("2d");
+const PLAYER_SIZE = 50;
 
-const fill_board = () => {  
+const fillBoard = () => {  
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, board.width, board.height);
 };
 
-const add_player = (id, color) => {
-  ctx.fillStyle = "red";
-  ctx.fillRect(0, 0, 100, 100);
-  const keysDown$ = Rx.Observable.fromEvent(document, 'keydown');
-  console.log(keysDown$);
+const pipe = (...functions) => (value) => {
+  return functions.reduce((currentValue, currentFunction) => {
+    return currentFunction(currentValue);
+  }, value);
 };
 
+const getRandomPos = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const addPlayer = (id, color) => {
+  ctx.fillStyle = color;
+  const x = getRandomPos(0, board.width - PLAYER_SIZE);
+  const y = getRandomPos(0, board.height - PLAYER_SIZE);
+  ctx.fillRect(x, y, PLAYER_SIZE, PLAYER_SIZE);
+};
+
+const input$ = Rx.Observable.fromEvent(document, 'keydown').subscribe((event) => console.log(event));
+
 document.addEventListener("DOMContentLoaded", () => {
-  fill_board();
-  add_player(1, "red");
+  pipe(
+    fillBoard,
+    addPlayer(1, "red"),
+    addPlayer(2, "green")
+  );
 });
