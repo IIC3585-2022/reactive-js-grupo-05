197fb9b05da0e3c6337a05ff63e36df7164fcd9b
elias <elias.sabja@uc.cl>
2022-04-06 19:20:35 -0400

    Uploaded sprites and pj random generation working

--- pacman.js
+++ pacman.js
@@ -1,6 +1,19 @@
 const board = document.getElementById("board");
 const ctx = board.getContext("2d");
 const PLAYER_SIZE = 50;
+const KEYMAP_PJ1 = {
+  left: 37,
+  up: 38,
+  right: 39,
+  down: 40,
+};
+
+const KEYMAP_PJ2 = {
+  left: 65,
+  up: 87,
+  right: 68,
+  down: 83,
+};
 
 const fillBoard = () => {  
   ctx.fillStyle = "black";
@@ -19,19 +32,21 @@ const getRandomPos = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
-const addPlayer = (id, color) => {
-  ctx.fillStyle = color;
+const addPlayer = (id) => {
   const x = getRandomPos(0, board.width - PLAYER_SIZE);
   const y = getRandomPos(0, board.height - PLAYER_SIZE);
-  ctx.fillRect(x, y, PLAYER_SIZE, PLAYER_SIZE);
+  const pj = document.getElementById("pj" + id);
+  pj.onload = () => ctx.drawImage(pj, x, y);
 };
 
-const input$ = Rx.Observable.fromEvent(document, 'keydown').subscribe((event) => console.log(event));
+const debug = () => {
+  return ctx.drawImage(document.getElementById("pj2"), 500, 500);
+};
+
+const input$ = Rx.Observable.fromEvent(document, 'keydown').subscribe((event) => console.log(event.keyCode));
 
 document.addEventListener("DOMContentLoaded", () => {
-  pipe(
-    fillBoard,
-    addPlayer(1, "red"),
-    addPlayer(2, "green")
-  );
+  fillBoard();
+  addPlayer(1);
+  addPlayer(2);
 });
