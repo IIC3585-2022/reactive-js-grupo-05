9519610b553a2b3300a213ebf1a0a0413bd133b3
elias <elias.sabja@uc.cl>
2022-04-08 10:35:57 -0400

    2 player movement

--- pacman.js
+++ pacman.js
@@ -52,41 +52,56 @@ const addPlayer = (id) => {
 
 const input$ = Rx.Observable.fromEvent(document, 'keydown').scan((lastMove, newMove) => {
   const nextMove = getMovement(lastMove, newMove.keyCode);
+  console.log(legalMove(lastMove, nextMove));
   return legalMove(lastMove, nextMove);
-}, { x: getRandomPos(0, board.width - PLAYER_WIDTH), y: getRandomPos(0, board.height - PLAYER_HEIGHT), dir: 0});
+}, { x: getRandomPos(0, board.width - PLAYER_WIDTH), y: getRandomPos(0, board.height - PLAYER_HEIGHT), dir: 0, pj:1})
+  .subscribe(newPos => movePJ(newPos));
 
-const movePlayer = input$.subscribe((newPos) => {
-  const pj = document.getElementById("pj1");
+const inputPJ2$ = Rx.Observable.fromEvent(document, 'keydown').scan((lastMove, newMove) => {
+  const nextMove = getMovement(lastMove, newMove.keyCode);
+  console.log(legalMove(lastMove, nextMove));
+  return legalMove(lastMove, nextMove);
+}, { x: getRandomPos(0, board.width - PLAYER_WIDTH), y: getRandomPos(0, board.height - PLAYER_HEIGHT), dir: 0, pj:2})
+  .subscribe(newPos => movePJ(newPos));
+
+const movePJ = (newPos) => {
+  const pj = document.getElementById("pj" + newPos.pj);
   ctx.fillRect(newPos.x + PREV_DIR[newPos.dir].x , newPos.y + PREV_DIR[newPos.dir].y, PLAYER_WIDTH, PLAYER_HEIGHT);
   ctx.drawImage(pj, newPos.x, newPos.y);
-});
+};
 
 const getMovement = (lastMove, keyCode) => {
+  let pj = 0;
+  if (Object.values(KEYMAP_PJ1).includes(keyCode)) {pj = 1;}
+  else if (Object.values(KEYMAP_PJ2).includes(keyCode)) {pj = 2};
+  
   if (keyCode == KEYMAP_PJ1.left || keyCode == KEYMAP_PJ2.left){
-    return {x: -VELOCITY, y: 0, dir: 'left'};
+    return {x: -VELOCITY, y: 0, dir: 'left', pj};
   } else if (keyCode == KEYMAP_PJ1.right || keyCode == KEYMAP_PJ2.right){
-    return {x: VELOCITY, y: 0, dir: 'right'};
+    return {x: VELOCITY, y: 0, dir: 'right', pj};
   } else if (keyCode == KEYMAP_PJ1.up || keyCode == KEYMAP_PJ2.up){
-    return {x: 0, y: -VELOCITY, dir:'up'};
+    return {x: 0, y: -VELOCITY, dir:'up', pj};
   } else if (keyCode == KEYMAP_PJ1.down || keyCode == KEYMAP_PJ2.down){
-    return {x: 0, y: +VELOCITY, dir:'down'};
+    return {x: 0, y: +VELOCITY, dir:'down', pj};
   }
-  return {x: 0, y:0, dir: 0};
+  return {x: 0, y:0, dir: 0, pj};
 };
 
 const legalMove = (lastMove, nextMove) => {
+  if (lastMove.pj != nextMove.pj) return lastMove;
   const dir = nextMove.dir
+  const pj = nextMove.pj
   if (lastMove.x + nextMove.x > board.width){
-    return {x: 0, y: lastMove.y, dir};
+    return {x: 0, y: lastMove.y, dir, pj};
   } else if (lastMove.x + nextMove.x < 0){
-    return {x: board.width - PLAYER_WIDTH, y: lastMove.y, dir};
+    return {x: board.width - PLAYER_WIDTH, y: lastMove.y, dir, pj};
   } else if (lastMove.y + nextMove.y > board.height){
-    return {x: lastMove.x, y: 0, dir: nextMove.dir};
+    return {x: lastMove.x, y: 0, dir, pj};
   } else if (lastMove.y + nextMove.y < 0){
-    return {x: lastMove.x, y: board.height - PLAYER_HEIGHT, dir};
+    return {x: lastMove.x, y: board.height - PLAYER_HEIGHT, dir, pj};
   }
 
-  return {x: lastMove.x + nextMove.x, y: lastMove.y + nextMove.y, dir};
+  return {x: lastMove.x + nextMove.x, y: lastMove.y + nextMove.y, dir, pj};
 };
 
 document.addEventListener("DOMContentLoaded", async () => {
